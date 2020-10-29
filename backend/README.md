6b38bf2e7463a4c6473b011ed17f4150b375a9ae
Clayton Campos <clayton.almeida.campos@gmail.com>
2020-10-29 10:10:58 -0300

    update aula 3

---
+++ backend/README.md
@@ -0,0 +1,11 @@
+## Bibliotecas / Linguagens
+
+- Typescript
+- Express
+- ts-node-dev
+- typeorm
+- sqlite3
+- multer (biblioteca para fazer upload de imagens)
+- express-async-errors
+- yup
+- cors
---
+++ backend/copia/src/database/migrations/1602764470489-create_orhhanages.ts
@@ -0,0 +1,62 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createOrhhanages1602764470489 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    //realiza alteracoes
+
+    await queryRunner.createTable(new Table({
+      name: 'orphanages',
+      columns: [
+        {
+          name: 'id',
+          type: 'integer',
+          unsigned: true,
+          isPrimary: true,
+          isGenerated: true,
+          generationStrategy: 'increment',
+        },
+        {
+          name: 'name',
+          type: 'varchar'
+        },
+        {
+          name: 'latitude',
+          type: 'decimal',
+          scale: 10,
+          precision: 2,
+        },
+        {
+          name: 'longitude',
+          type: 'decimal',
+          scale: 10,
+          precision: 2,
+        },
+        {
+          name: 'about',
+          type: 'text',
+        },
+        {
+          name: 'instructions',
+          type: 'text',
+        },
+        {
+          name: 'opening_hours',
+          type: 'varchar',
+        },
+        {
+          name: 'open_on_weekends',
+          type: 'boolean',
+          default: false,
+        },
+      ],
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    //desfazer o que foi feito metodo up
+
+    await queryRunner.dropTable('orphanages');
+  }
+
+}
